a29563d975a3c7564da58744a8db051145918cab
fliderman01 <fliderman01@gmail.com>
2025-10-22 17:22:55 +0400

    refactor: solve modules task

---
+++ src/modules/esm.mjs
@@ -0,0 +1,36 @@
+import path from "node:path";
+import { createServer } from "node:http";
+import { release, version } from "node:os";
+
+import "./files/c.cjs";
+
+const random = Math.random();
+
+const unknownObject =
+  random > 0.5
+    ? await import("./files/a.json", { with: { type: "json" } })
+    : await import("./files/b.json", { with: { type: "json" } });
+
+console.log(`Release ${release()}`);
+console.log(`Version ${version()}`);
+console.log(`Path segment separator is "${path.sep}"`);
+
+console.log(`Path to current file is ${import.meta.url}`);
+console.log(
+  `Path to current directory is ${new URL(".", import.meta.url).pathname}`
+);
+
+const myServer = createServer((_, res) => {
+  res.end("Request accepted");
+});
+
+const PORT = 3000;
+
+console.log(unknownObject);
+
+myServer.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+  console.log("To terminate it, use Ctrl+C combination");
+});
+
+export { unknownObject, myServer };

4f2909cf035c593f653de9d140089d019f253ae0
fliderman01 <fliderman01@gmail.com>
2025-10-22 16:49:22 +0400

    refactor: solve cli tasks

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,36 @@
+import fs from "fs";
+import path from "path";
+
+function loadEnvs() {
+  const envPath = path.resolve(process.cwd(), ".env");
+  const envContent = fs.readFileSync(envPath, "utf8");
+
+  envContent.split("\n").forEach((i) => {
+    i = i.trim();
+
+    const equalIndex = i.indexOf("=");
+    if (equalIndex === -1) {
+      return;
+    }
+
+    const envKey = i.substring(0, equalIndex).trim();
+    let value = i.substring(equalIndex + 1).trim();
+
+    if (!process.env[envKey]) {
+      process.env[envKey] = value;
+    }
+  });
+}
+
 const parseEnv = () => {
-  // Write your code here
+  loadEnvs();
+
+  const rssVars = Object.keys(process.env)
+    .filter((i) => i.startsWith("RSS_"))
+    .map((i) => `${i}=${process.env[i]}`)
+    .join("; ");
+
+  console.log(rssVars);
 };
 
 parseEnv();

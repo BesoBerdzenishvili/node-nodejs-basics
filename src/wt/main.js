7ac6e51f0fd5845d0cd3e6b341d0063dcfe5bd2d
fliderman01 <fliderman01@gmail.com>
2025-10-22 21:45:39 +0400

    refactor: solve wt tasks

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
+import { cpus } from "node:os";
+import { Worker } from "node:worker_threads";
+
 const performCalculations = async () => {
-  // Write your code here
+  const output = [];
+  const cpusLength = cpus().length;
+
+  const add = (i, indx) => {
+    return new Promise((resolve) => {
+      const fibonacci = new Worker("./src/wt/worker.js");
+      fibonacci.on("message", (j) => {
+        output[indx] = j;
+        resolve();
+      });
+      fibonacci.on("error", () => {
+        output[indx] = { status: "error", data: null };
+        resolve();
+      });
+      fibonacci.postMessage(i);
+    });
+  };
+
+  const arr = [];
+  for (let i = 0; i < cpusLength; i++) {
+    arr.push(add(10 + i, i));
+  }
+  await Promise.all(arr);
+  console.log(output);
 };
 
 await performCalculations();

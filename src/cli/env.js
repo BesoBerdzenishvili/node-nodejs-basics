29bbd9766f8194ad4babc7027605a959520d8c2c
fliderman01 <fliderman01@gmail.com>
2025-10-22 22:23:11 +0400

    fix: env not getting args

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,36 +1,12 @@
-import fs from "fs";
-import path from "path";
+import process from "node:process";
 
-function loadEnvs() {
-  const envPath = path.resolve(process.cwd(), ".env");
-  const envContent = fs.readFileSync(envPath, "utf8");
-
-  envContent.split("\n").forEach((i) => {
-    i = i.trim();
-
-    const equalIndex = i.indexOf("=");
-    if (equalIndex === -1) {
-      return;
-    }
-
-    const envKey = i.substring(0, equalIndex).trim();
-    let value = i.substring(equalIndex + 1).trim();
-
-    if (!process.env[envKey]) {
-      process.env[envKey] = value;
-    }
-  });
+function parseEnv() {
+  console.log(
+    Object.entries(process.env)
+      .filter(([i]) => i.startsWith("RSS_"))
+      .map(([i, j]) => `${i}=${j}`)
+      .join("; ")
+  );
 }
-
-const parseEnv = () => {
-  loadEnvs();
-
-  const rssVars = Object.keys(process.env)
-    .filter((i) => i.startsWith("RSS_"))
-    .map((i) => `${i}=${process.env[i]}`)
-    .join("; ");
-
-  console.log(rssVars);
-};
 
 parseEnv();

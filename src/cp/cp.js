4ab87f546a516e55dd47f85ea8ddb605f9c5571e
fliderman01 <fliderman01@gmail.com>
2025-10-22 22:16:19 +0400

    refactor: solve cp task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,13 @@
+import { spawn } from "node:child_process";
+
 const spawnChildProcess = async (args) => {
-  // Write your code here
+  const spawnProcess = spawn("node", ["./src/cp/files/script.js", ...args], {
+    stdio: ["pipe", "pipe", "inherit"],
+  });
+
+  spawnProcess.stdin.pipe(spawnProcess.stdin);
+
+  spawnProcess.stdout.pipe(process.stdout);
 };
 
-// Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(["IU", "Sunmi", "V", "PSY", "Hanni", "D.O."]);
